f9cc39f852961d2bca693c792374ef6b3ed67c92
xdFunnt <tgachengo@gmail.com>
2024-10-21 12:39:38 +0100

    Update url.py
    simplified

--- version_2/url.py
+++ version_2/url.py
@@ -7,7 +7,6 @@ class URL:
     scheme://host/path    
     '''
     def __init__(self, url):
-        #self.connection = "close"
         self.scheme, url = url.split("://",1)
         assert self.scheme in ["http", "https"]
         if self.scheme == "http":
@@ -41,7 +40,7 @@ class URL:
 
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
-        # request += "Connection: {}\r\n".format(self.connection)
+        # request += "Connection: close\r\n"
         request += "\r\n"
         s.send(request.encode("utf8"))
         
